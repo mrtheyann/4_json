56d9c8347b08dbcf8e2518bd5a7a83127bfa6233
mrtheyann <darklodr@gmail.com>
2017-04-28 23:54:25 +0600

    upd: solution implemented

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,30 @@
 import json
+from sys import argv
 
 
-def load_data(filepath):
-    pass
+def load_json_input(filepath):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        json_input = json.load(f)
+    return json_input
 
 
-def pretty_print_json(data):
+def pretty_print_json(json_input):
+    pretty_json = json.dumps(json_input, sort_keys=True, indent=4, ensure_ascii=False)
+    return pretty_json
+
+
+def main():
+    try:
+        json_input = load_json_input(argv[1])
+        print(pretty_print_json(json_input))
+    except IndexError:
+        print('Invalid filepath')
+    except FileNotFoundError:
+        print('File not found')
+    except json.JSONDecodeError as err:
+        print('Bad json: {}'.format(err))
     pass
 
 
 if __name__ == '__main__':
-    pass
+    main()
